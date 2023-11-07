b4ca3600e270e44b84d07c74e754468832f13d33
NichoUlloa <n.ulloa06@ufromail.cl>
2023-11-07 17:17:44 -0300

    Se agrega la clase Persona y se usa como herencia para Cliente y Vendedor, tambien se actualiza la clase Principal,

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -25,3 +25,4 @@ public class Cliente extends Persona {
                 "Direccion: " + direccionCliente + "\n";
     }
 }
+
--- src/main/java/Vendedor.java
+++ src/main/java/Vendedor.java
@@ -25,3 +25,4 @@ public class Vendedor extends Persona {
                 "Antiguedad: " + antiguedadVendedorAnios + "\n";
     }
 }
+
